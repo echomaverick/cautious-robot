64766f50329f25d6503c60faa7c4ae0a4999d5b6
Samuel <samueld@tegeria.com>
2024-07-25 17:55:05 +0200

    minor bug fixing

--- socialApp-client/src/components/Login.jsx
+++ socialApp-client/src/components/Login.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Spinner } from "react-bootstrap";
 import image from "../assets/logo.png";
 import "../styles/login.css";
 
 const LoginScript = () => {
   const navigate = useNavigate();
   const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const apiUrl = import.meta.env.VITE_API_BASE_URL;
 
@@ -20,20 +22,27 @@ const LoginScript = () => {
           if (Date.now() > expirationTime) {
             localStorage.removeItem("token");
             console.log("Token expired, user logged out");
+            setLoading(false);
           } else {
-            navigate("/home");
+            setTimeout(() => {
+              navigate("/home");
+            }, 2000);
           }
         } catch (error) {
           console.error("Error decoding token:", error.message);
+          setLoading(false);
         }
+      } else {
+        setLoading(false);
       }
     };
 
     checkTokenExpiration();
-  }, []);
+  }, [navigate]);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
+    setLoading(true);
 
     const username = event.target.username.value;
     const password = event.target.password.value;
@@ -50,69 +59,81 @@ const LoginScript = () => {
       if (response.ok) {
         const token = await response.text();
         localStorage.setItem("token", token);
-        navigate("/home");
+        setTimeout(() => {
+          navigate("/home");
+        }, 3000); // Navigate after 3 seconds
       } else {
         const errorMessage = await response.text();
         setError(errorMessage);
+        setLoading(false);
       }
     } catch (error) {
       console.error("Error:", error.message);
       setError("An unexpected error occurred. Please try again later.");
+      setLoading(false);
     }
   };
 
   return (
     <form id="loginForm" onSubmit={handleSubmit}>
-      <div className="screen">
-        <div className="half-screen">
-          <img src={image} alt="" className="hello-image" />
+      {loading ? (
+        <div className="spinner-container">
+          <Spinner animation="border" role="status">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
         </div>
-        <div className="container-sign">
-          {error && <p className="error-message">{error}</p>}
+      ) : (
+        <div className="screen">
+          <div className="half-screen">
+            <img src={image} alt="" className="hello-image" />
+          </div>
+          <div className="container-sign">
+            {error && <p className="error-message">{error}</p>}
 
-          <label htmlFor="username">
-            <b>Username</b>
-          </label>
-          <input
-            type="text"
-            placeholder="Enter Username"
-            name="username"
-            id="username"
-            required
-          />
+            <label htmlFor="username">
+              <b>Username</b>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Username"
+              name="username"
+              id="username"
+              required
+            />
 
-          <label htmlFor="password">
-            <b>Password</b>
-          </label>
-          <input
-            type="password"
-            placeholder="Enter Password"
-            name="password"
-            id="password"
-            required
-          />
+            <label htmlFor="password">
+              <b>Password</b>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              name="password"
+              id="password"
+              required
+            />
 
-          <button
-            style={{
-              backgroundColor: "#04aa6d",
-              color: "white",
-              padding: "16px 20px",
-              margin: "8px 0",
-              border: "none",
-              cursor: "pointer",
-              width: "100%",
-              opacity: "0.9",
-              borderRadius: "20px",
-            }}
-            type="submit"
-          >
-            Login
-          </button>
-          <p className="signup">
-            Don't have an account? <a href="/register">Sign up</a>.
-          </p>
+            <button
+              style={{
+                backgroundColor: "#04aa6d",
+                color: "white",
+                padding: "16px 20px",
+                margin: "8px 0",
+                border: "none",
+                cursor: "pointer",
+                width: "100%",
+                opacity: "0.9",
+                borderRadius: "20px",
+              }}
+              type="submit"
+            >
+              Login
+            </button>
+            <p className="signup">
+              Don't have an account? <a href="/register">Sign up</a>.
+            </p>
+          </div>
         </div>
-      </div>
+      )}
     </form>
   );
 };
